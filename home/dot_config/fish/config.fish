37e30d44f1840dfc6b448362e348c14a73641402
Ottomated <otto@ottomated.net>
2025-04-18 13:58:54 -0700

    move path to .profile

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -12,12 +12,4 @@ end
 alias cat="bat"
 alias ls="eza"
 
-set -gx PATH "$HOME/.cargo/bin" $PATH
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-
 zoxide init fish --cmd cd | source
