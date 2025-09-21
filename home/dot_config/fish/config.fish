811f85812d03f7bbc0ff2ec5e891dd410d30ba04
Ottomated <otto@ottomated.net>
2025-09-20 21:28:51 -0700

    volta -> pnpm

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -13,3 +13,10 @@ alias cat="bat"
 alias ls="eza"
 
 zoxide init fish --cmd cd | source
+
+# pnpm
+set -gx PNPM_HOME "/home/otto/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
